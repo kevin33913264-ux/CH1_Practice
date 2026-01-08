3d2246b3593ae78fde14126bbf54b03926273f1b
kevin33913264-ux <kevin33913264@gmail.com>
2026-01-08 12:08:46 +0800

    Add P09 and P10

---
+++ src/P09_PrintDemo.java
@@ -0,0 +1,6 @@
+public class P09_PrintDemo {
+    public static void main(String[] args) {
+        System.out.print("歡迎使用Java!");
+        System.out.print("開始學Java吧!");
+    }
+}
---
+++ src/P10_PrintlnDemo.java
@@ -0,0 +1,6 @@
+public class P10_PrintlnDemo {
+    public static void main(String[] args) {
+        System.out.println("歡迎使用Java!");
+        System.out.println("開始學Java吧!");
+    }
+}
